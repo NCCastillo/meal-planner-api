fbb51ac3b7c08a5c4ee7eabbd2c806e33fcf3953
Nestor Castillo <nes_23@yahoo.com>
2017-01-15 13:40:12 -0700

    Add email validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,30 @@ require "rails_helper"
 describe User, type: :model do
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_presence_of(:email) }
+
+  context "when a valid email format is given" do 
+    it "returns true" do 
+      user = User.new(email: "john@example.com", password: "test", password_confirmation: "test")
+
+      expect(user.valid?).to be true
+    end
+  end
+  
+  context "when an invalid email format is given" do 
+    context "such as a missing @" do 
+      it "returns false" do 
+        user = User.new(email: "john.com", password: "test", password_confirmation: "test")
+        
+        expect(user.invalid?).to be true 
+      end
+    end
+
+    context "such as a missing ." do 
+      it "returns false" do 
+        user = User.new(email: "john@examplecom", password: "test", password_confirmation: "test")
+        
+        expect(user.invalid?).to be true 
+      end
+    end
+  end
 end
