f88e235a2a43abcf68aeb5c994f5f2a420ed3fe8
Nestor Castillo <nes_23@yahoo.com>
2017-02-11 15:23:36 -0700

    change associations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,4 @@
 class Meal < ApplicationRecord
-  has_many :menus
   has_many :meal_ingredients
   has_many :ingredients, through: :meal_ingredients
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Meal, type: :model do
-  it { is_expected.to have_many(:menus) }
-  it { is_expected.to have_many(:meal_ingredients) }
-  it { is_expected.to have_many(:ingredients).through(:meal_ingredients) }
+describe Meal, type: :model do
+  context "Associations" do 
+    it { is_expected.to have_many(:meal_ingredients) }
+    it { is_expected.to have_many(:ingredients).through(:meal_ingredients) }
+  end
 end
